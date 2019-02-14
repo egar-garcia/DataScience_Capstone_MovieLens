8f04e295d31a9588ebcc3afe9a9a485af9b726fa
Egar Garcia <egar.garcia@gmail.com>
2019-02-13 23:18:21 -0800

    Progress on 2019-02-13 23:18

--- report_generator.R
+++ report_generator.R
@@ -1,3 +1,6 @@
 require(rmarkdown)
 
-render('Report.Rmd', pdf_document(number_sections = TRUE))
+render('Report.Rmd',
+       pdf_document(number_sections = TRUE,
+                    fig_width = 4,
+                    fig_height = 3))
